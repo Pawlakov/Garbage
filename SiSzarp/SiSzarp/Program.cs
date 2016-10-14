d2a070c7795a406390d0cde2ed61cd0aed1cefcb
Pawlakov <pmatusek1@wp.pl>
2016-10-14 10:31:23 +0200

    Does a thing

--- SiSzarp/SiSzarp/Program.cs
+++ SiSzarp/SiSzarp/Program.cs
@@ -2,8 +2,21 @@
 
 class Program
 {
-    static void Main()
+    public static void Main()
     {
+        int x;
+        int y;
 
+        Console.Write("Podaj x: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Podaj y: ");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Suma: " + (x + y));
+        Console.WriteLine("Roznica: " + (x - y));
+        Console.WriteLine("Iloczyn: " + (x * y));
+        Console.WriteLine("Iloraz: " + (x / y));
+        Console.WriteLine("Minimum: " + Math.Min(x, y));
+        Console.WriteLine("Maksimum: " + Math.Max(x, y));
+        Console.Read();
     }
 }
